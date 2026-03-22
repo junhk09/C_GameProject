10cfc5b39795f77ee414869704bbd877f94f9790
junhk09 <black09248@gmail.com>
2026-03-22 14:36:20 +0900

    시작화면 추가

--- prac.c
+++ prac.c
@@ -9,7 +9,8 @@ const int SCREEN_W = 1280;
 const int SCREEN_H = 720;
 const int GROUND_Y = 540; 
 
-typedef enum { STATE_PLAYING, STATE_GAME_OVER, STATE_VICTORY } GameState;
+// ★ STATE_START 추가
+typedef enum { STATE_START, STATE_PLAYING, STATE_GAME_OVER, STATE_VICTORY } GameState;
 typedef enum { IDLE, RUN, ATTACK, JUMP, HURT, DIE } PlayerState;
 typedef enum { M_IDLE, M_ATTACK, M_HURT, M_DEAD } MonsterAnim;
 typedef enum { S_GREEN, S_BLUE, S_RED, S_YELLOW } SlimeColor;
@@ -41,7 +42,7 @@ struct Portal { float x, y; bool active; } exitPortal = {1000, 380, false};
 struct Monster monsters[3];
 int currentStage = 1;
 int monsterCount = 1;
-GameState gameState = STATE_PLAYING;
+GameState gameState = STATE_START; // ★ 초기 상태를 시작 화면으로 설정
 
 void checkLevelUp() {
     if (player.lv < 3 && player.exp >= 2) {
@@ -82,7 +83,7 @@ void drawText(SDL_Renderer* ren, TTF_Font* font, const char* text, int x, int y,
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO); IMG_Init(IMG_INIT_PNG); TTF_Init();
-    SDL_Window* win = SDL_CreateWindow("C-RPG: Fixed AI & Knockback", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_W, SCREEN_H, 0);
+    SDL_Window* win = SDL_CreateWindow("Action-RPG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_W, SCREEN_H, 0);
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
     TTF_Font* font = TTF_OpenFont("font.ttf", 28);
@@ -103,7 +104,11 @@ int main(int argc, char* argv[]) {
         while (SDL_PollEvent(&ev)) {
             if (ev.type == SDL_QUIT) running = false;
             if (ev.type == SDL_KEYDOWN) {
-                if (gameState == STATE_PLAYING && player.hp > 0) {
+                // ★ 시작 화면 처리
+                if (gameState == STATE_START) {
+                    if (ev.key.keysym.sym == SDLK_s) gameState = STATE_PLAYING;
+                }
+                else if (gameState == STATE_PLAYING && player.hp > 0) {
                     if (ev.key.keysym.sym == SDLK_a && player.attack_timer <= 0) {
                         player.attack_timer = 30; player.state = ATTACK; player.frame = 0;
                         for(int i=0; i<monsterCount; i++) {
@@ -112,10 +117,8 @@ int main(int argc, char* argv[]) {
                                 if (fabs((player.x + 90) - (monsters[i].x + monsters[i].size/2)) < range && 
                                     fabs((player.y + 100) - (monsters[i].y + monsters[i].size/2)) < 300) {
                                     monsters[i].hp -= player.atk;
-                                    
-                                    // ★ 슬라임 넉백 효과 추가 (맞으면 플레이어 반대 방향으로 밀려남)
+                                    // 넉백 수치 25로 조정
                                     monsters[i].x += (player.x < monsters[i].x) ? 25 : -25;
-
                                     if (monsters[i].hp <= 0) { monsters[i].animState = M_DEAD; monsters[i].frame = 0; player.exp++; checkLevelUp(); }
                                     else { monsters[i].animState = M_HURT; monsters[i].hurt_timer = 20; monsters[i].frame = 0; }
                                 }
@@ -127,14 +130,15 @@ int main(int argc, char* argv[]) {
                         for(int i=0; i<monsterCount; i++) {
                             if (monsters[i].alive && monsters[i].animState != M_DEAD) {
                                 monsters[i].hp -= 250; 
-                                monsters[i].x += (player.x < monsters[i].x) ? 30 : -30; // 궁극기는 넉백도 2배!
+                                monsters[i].x += (player.x < monsters[i].x) ? 50 : -50;
                                 if (monsters[i].hp <= 0) { monsters[i].animState = M_DEAD; monsters[i].frame = 0; player.exp++; checkLevelUp(); }
                                 else { monsters[i].animState = M_HURT; monsters[i].hurt_timer = 30; }
                             }
                         }
                     }
                     if (ev.key.keysym.sym == SDLK_SPACE && player.isGrounded && player.attack_timer <= 0) { player.dy = -22.0f; player.isGrounded = false; }
-                } else if (ev.key.keysym.sym == SDLK_r) { 
+                } 
+                else if (ev.key.keysym.sym == SDLK_r) { 
                     currentStage = 1; initStage(1);
                     player = (struct Player){100, 440, 0, 100, 100, 1, 0, 40, IDLE, 0, 0, 8, 0, 0, 0, 0, false, true, SDL_FLIP_NONE, 100};
                     gameState = STATE_PLAYING;
@@ -175,24 +179,16 @@ int main(int argc, char* argv[]) {
                     monsters[i].hurt_timer--;
                     monsters[i].timer++;
                     if (monsters[i].timer >= 8) { monsters[i].frame = (monsters[i].frame + 1) % 4; monsters[i].timer = 0; }
-                    
-                    // ★ 피격 타이머 종료 시 다시 IDLE 상태로 복귀하여 추격하게 함
-                    if (monsters[i].hurt_timer <= 0) {
-                        monsters[i].animState = M_IDLE;
-                        monsters[i].frame = 0;
-                    }
+                    if (monsters[i].hurt_timer <= 0) { monsters[i].animState = M_IDLE; monsters[i].frame = 0; }
                 }
                 else {
                     float dist = fabs((player.x + 90) - (monsters[i].x + monsters[i].size/2));
                     float stopDist = monsters[i].isBoss ? 180.0f : 110.0f;
-                    
-                    // 추격 로직
                     if (dist > stopDist && monsters[i].animState != M_ATTACK) {
                         if (monsters[i].x < player.x) monsters[i].x += monsters[i].speed; else monsters[i].x -= monsters[i].speed;
                         monsters[i].animState = M_IDLE; 
                     } else if (monsters[i].animState == M_IDLE) {
-                        monsters[i].animState = M_ATTACK;
-                        monsters[i].frame = 0;
+                        monsters[i].animState = M_ATTACK; monsters[i].frame = 0;
                     }
 
                     monsters[i].timer++; 
@@ -233,13 +229,20 @@ int main(int argc, char* argv[]) {
 
         SDL_RenderClear(ren);
         SDL_Rect bgRect = {0, 0, SCREEN_W, SCREEN_H};
-        SDL_RenderCopy(ren, (currentStage == 1 ? bgTex1 : (currentStage == 2 ? bgTex2 : bgTexBoss)), NULL, &bgRect);
 
-        if (gameState != STATE_PLAYING) {
+        // ★ 시작 화면 렌더링
+        if (gameState == STATE_START) {
+            SDL_RenderCopy(ren, bgTex1, NULL, &bgRect); // 배경은 스테이지 1 배경 활용
+            drawText(ren, bigFont, "Action-RPG", SCREEN_W/2 - 250, SCREEN_H/2 - 120, (SDL_Color){255, 255, 255});
+            drawText(ren, font, "Press 'S' to Start", SCREEN_W/2 - 150, SCREEN_H/2 + 40, (SDL_Color){255, 255, 0});
+        } 
+        else if (gameState != STATE_PLAYING) {
+            SDL_RenderCopy(ren, (currentStage == 1 ? bgTex1 : (currentStage == 2 ? bgTex2 : bgTexBoss)), NULL, &bgRect);
             SDL_Color col = (gameState == STATE_VICTORY) ? (SDL_Color){0,255,0} : (SDL_Color){255,0,0};
             drawText(ren, bigFont, (gameState == STATE_VICTORY) ? "VICTORY!" : "GAME OVER", SCREEN_W/2 - 200, SCREEN_H/2 - 100, col);
             drawText(ren, font, "Press 'R' to Restart", SCREEN_W/2 - 120, SCREEN_H/2 + 50, (SDL_Color){255,255,255});
         } else {
+            SDL_RenderCopy(ren, (currentStage == 1 ? bgTex1 : (currentStage == 2 ? bgTex2 : bgTexBoss)), NULL, &bgRect);
             if (exitPortal.active) {
                 SDL_Rect pR = {(int)exitPortal.x, (int)exitPortal.y, 100, 180};
                 if (portalTex) SDL_RenderCopy(ren, portalTex, NULL, &pR);
